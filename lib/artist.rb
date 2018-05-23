0fedd3f7caa6d8df4ad7c59c6ff2f498ebf63c9a
Carrie Savinell <carrielane89@gmail.com>
2018-05-23 22:03:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,33 @@
-class Artist 
+class Artist
 
   attr_accessor :name
-  
-  def initialize(name)
-    @name = name 
+  @@song_count = 0
 
-  end   
-    
-end   
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song)
+    song = Song.new(song)
+    self.add_song(song)
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+end 
 
 
 
